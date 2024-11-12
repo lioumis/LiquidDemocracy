2bd4fcebf1c4261c947e827f3dc6f2aed0d477d6
Evangelos Lioumis <Evangelos.Lioumis@arhs-dev-hellas.com>
2024-11-12 22:03:36 +0200

    Initial

---
+++ liquid-democracy-app/src/main/java/gr/upatras/ceid/ld/LiquidDemocracyApplication.java
@@ -0,0 +1,13 @@
+package gr.upatras.ceid.ld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidDemocracyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidDemocracyApplication.class, args);
+	}
+
+}
---
+++ liquid-democracy-app/src/test/java/gr/upatras/ceid/ld/LiquidDemocracyApplicationTests.java
@@ -0,0 +1,13 @@
+package gr.upatras.ceid.ld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiquidDemocracyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
